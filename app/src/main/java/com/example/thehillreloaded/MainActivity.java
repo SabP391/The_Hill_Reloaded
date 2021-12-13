81a3c32149052dfa0d7cb1084b87d06305a163e0
bullseye33 <50544102+bullseye33@users.noreply.github.com>
2021-12-13 18:59:16 +0100

    Il sole gira!!!!

--- app/src/main/java/com/example/thehillreloaded/MainActivity.java
+++ app/src/main/java/com/example/thehillreloaded/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.thehillreloaded;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RotateAnimation animazioneSole = new RotateAnimation(0.0f, 360.0f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        animazioneSole.setInterpolator(new LinearInterpolator());
+        animazioneSole.setRepeatCount(Animation.INFINITE);
+        animazioneSole.setDuration(7000);
+
+// Start animating the image
+        final ImageView soleRotante = (ImageView) findViewById(R.id.sole_rotante_sp);
+        soleRotante.startAnimation(animazioneSole);
+
     }
 }
