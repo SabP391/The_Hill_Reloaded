84f08346aa7579bc7b4ebe6152af639c3b0e7a29
bullseye33 <50544102+bullseye33@users.noreply.github.com>
2021-12-14 19:58:33 +0100

    Nuovo fragment per selezione modalità

--- app/src/main/java/com/example/thehillreloaded/UserMenuActivity.java
+++ app/src/main/java/com/example/thehillreloaded/UserMenuActivity.java
@@ -1,8 +1,13 @@
 package com.example.thehillreloaded;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserMenuActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class UserMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_menu);
+    }
+
+    public void onClickNuovaPartita(View view) {
+        selezionaModalitàFragment(new GameModeFragment());
+    }
+
+    private void selezionaModalitàFragment(Fragment fragment){
+        //Crea il fragmentmanager
+        FragmentManager fragmentManager = getFragmentManager();
+        //Crea il fragmenttransaction
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
