bec3b983bdc28d018101ae9de1aece00e77f546b
rossixal <76973763+rossixal@users.noreply.github.com>
2021-12-14 10:30:09 +0100

    Aggiunta della seconda activity

--- app/src/main/java/com/example/thehillreloaded/MainActivity.java
+++ app/src/main/java/com/example/thehillreloaded/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.thehillreloaded;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
@@ -15,16 +17,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Inizializzazione dell'animazione del sole
         RotateAnimation animazioneSole = new RotateAnimation(0.0f, 360.0f,
                 Animation.RELATIVE_TO_SELF, 0.5f,
                 Animation.RELATIVE_TO_SELF, 0.5f);
         animazioneSole.setInterpolator(new LinearInterpolator());
+        // setRepeatCount è impostato su Animation.INFINITE per ripetere l'animazione in loop
         animazioneSole.setRepeatCount(Animation.INFINITE);
         animazioneSole.setDuration(7000);
 
-// Start animating the image
+        // Inizia l'animazione del sole che ruota
         final ImageView soleRotante = (ImageView) findViewById(R.id.sole_rotante_sp);
         soleRotante.startAnimation(animazioneSole);
+
+        //Creazione dell'intent per lanciare la schermata successiva
+        Intent nuovaActivity = new Intent(this, AccessActivity.class);
+
+        //funzione temporanea che serve a passare alla schermata successiva
+        //tappando sul sole
+        soleRotante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(nuovaActivity);
+            }
+        });
 
     }
 }
