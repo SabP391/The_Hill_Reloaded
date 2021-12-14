2335fd566d8e2725d181d781f65d486c953a848e
SabP391 <sabpinto91@gmail.com>
2021-12-14 17:21:39 +0100

    Nuove activity e intent per i menu principali

--- app/src/main/java/com/example/thehillreloaded/AccessActivity.java
+++ app/src/main/java/com/example/thehillreloaded/AccessActivity.java
@@ -2,13 +2,28 @@ package com.example.thehillreloaded;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AccessActivity extends AppCompatActivity {
+
+    //Creazione dell'intent per lanciare il menu con autenticazione
+    Intent menuUtente;
+    //Creazione dell'intent per lanciare il menu senza autenticazione
+    Intent menuOspite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_access);
+
+        menuUtente = new Intent(this, UserMenuActivity.class);
+        menuOspite = new Intent(this, GuestMenuActivity.class);
+    }
+
+    public void onClickUtente(View view) { startActivity(menuUtente); }
+    public void onClickOspite(View view) {
+        startActivity(menuOspite);
     }
 }
---
+++ app/src/main/java/com/example/thehillreloaded/GuestMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.thehillreloaded;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuestMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_menu);
+    }
+}
