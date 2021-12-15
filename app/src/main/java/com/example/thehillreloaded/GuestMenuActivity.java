c4fe715557689e0862a23e18488d72ace36abc9f
SabP391 <sabpinto91@gmail.com>
2021-12-15 18:16:49 +0100

    Fragment nel menu Guest e modifica di TUTTI i bottoni (tranne quelli che erano già modificati)

--- app/src/main/java/com/example/thehillreloaded/GuestMenuActivity.java
+++ app/src/main/java/com/example/thehillreloaded/GuestMenuActivity.java
@@ -1,14 +1,19 @@
 package com.example.thehillreloaded;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class GuestMenuActivity extends AppCompatActivity {
 
     Intent menuImpostazioni;
+    Button nuovaPartita;
+    Fragment modalitaGioco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,32 @@ public class GuestMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_guest_menu);
 
         menuImpostazioni = new Intent(this, SettingsActivity.class);
+
+        nuovaPartita = findViewById(R.id.bottone_inizia_ospite);
+        nuovaPartita.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                modalitaGioco = new GameModeFragment();
+                selezionaModalitàFragment(modalitaGioco);
+            }
+        });
+    }
+
+
+    private void selezionaModalitàFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack("fragment1")
+                .add(R.id.fragment_modalita_ospite, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0)
+            getSupportFragmentManager().popBackStackImmediate();
+        else super.onBackPressed();
     }
 
     public void onClickImpostazioni(View view) { startActivity(menuImpostazioni); }
