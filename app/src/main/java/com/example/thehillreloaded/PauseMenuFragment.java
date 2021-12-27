2b379f674135a7d87a68f3664bed9efe94570299
SabP391 <sabpinto91@gmail.com>
2021-12-27 12:19:34 +0100

    Aggiunta icone per switch (musica e sfx) e gestione suoni dal menu di pausa (in-game)

--- app/src/main/java/com/example/thehillreloaded/PauseMenuFragment.java
+++ app/src/main/java/com/example/thehillreloaded/PauseMenuFragment.java
@@ -1,5 +1,9 @@
 package com.example.thehillreloaded;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +11,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.example.thehillreloaded.Services.BGMusicService;
+import com.example.thehillreloaded.Services.SoundEffectService;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +23,16 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class PauseMenuFragment extends Fragment {
+    private Switch musicaBottone;
+    boolean statoMusica;
+    private Switch effettiBottone;
+    boolean SFXattivi;
+    SoundEffectService soundService;
+    Intent avviaMusica;
+    //variabili per le SharedPreferences
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,7 +77,61 @@ public class PauseMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pause_menu, container, false);
+        View view = inflater.inflate(R.layout.fragment_pause_menu, container, false);
+
+        avviaMusica = new Intent(getActivity().getApplicationContext(), BGMusicService.class);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        //getSharedPreferences può essere chiamato solo DOPO l'onCreate di un'attività
+        pref = getActivity().getApplicationContext().getSharedPreferences("HillR_pref", Context.MODE_PRIVATE);
+        editor = pref.edit();
+        effettiBottone = (Switch) getView().findViewById(R.id.switch_sfx_ingame);
+        musicaBottone = (Switch) getView().findViewById(R.id.switch_musica_ingame);
+
+        SFXattivi = pref.getBoolean("SFX_attivi", true);
+        statoMusica = pref.getBoolean("Musica_attiva", true);
+        musicaBottone.setChecked(statoMusica);
+        musicaBottone.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (musicaBottone.isChecked()) {
+                    //attiva la musica in background e salva la scelta nelle SharedPreferences
+                    getActivity().startService(avviaMusica);
+                    statoMusica = true;
+                    editor.putBoolean("Musica_attiva", true);
+                    editor.apply();
+                } else if (!musicaBottone.isChecked()) {
+                    //ferma la musica in background e salva la scelta nelle SharedPreferences
+                    getActivity().stopService(avviaMusica);
+                    statoMusica = false;
+                    editor.putBoolean("Musica_attiva", false);
+                    editor.apply();
+                }
+            }
+        });
+        effettiBottone.setChecked(SFXattivi);
+        effettiBottone.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (effettiBottone.isChecked()) {
+                    //attiva gli effetti sonori e salva la scelta nelle SharedPreferences
+                    editor.putBoolean("SFX_attivi", true);
+                    editor.apply();
+                    SFXattivi = pref.getBoolean("SFX_attivi", true);
+                } else if (!effettiBottone.isChecked()) {
+                    //disattiva gli effetti sonori e salva la scelta nelle SharedPreferences
+                    editor.putBoolean("SFX_attivi", false);
+                    editor.apply();
+                    SFXattivi = pref.getBoolean("SFX_attivi", true);
+                }
+            }
+        });
+
     }
 }
