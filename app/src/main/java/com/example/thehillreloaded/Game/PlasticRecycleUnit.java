63301b2e39f81f0067c8de338886fa18b0c8f1bd
bullseye33 <50544102+bullseye33@users.noreply.github.com>
2021-12-28 11:33:38 +0100

    Posizionamento centrali di riciclo nella tilemap

--- app/src/main/java/com/example/thehillreloaded/Game/PlasticRecycleUnit.java
+++ app/src/main/java/com/example/thehillreloaded/Game/PlasticRecycleUnit.java
@@ -8,7 +8,7 @@ public class PlasticRecycleUnit extends RecycleUnit{
     public PlasticRecycleUnit(TileMap map, Context context){
         super(map, context);
         this.sprite = GameAssets.getInstance(context).getPlasticUnit(size);
-        position = new Point(0, (int)(map.getTileSize()));
+        position = new Point((int)(map.getTileSize())*3, (int)(map.getTileSize())*5);
 
     }
 }
