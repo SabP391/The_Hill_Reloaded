b03f96f79bce77280be189449be00e3cc896ae0e
rossixal <76973763+rossixal@users.noreply.github.com>
2022-01-03 11:45:37 +0100

    Aggiunto metodo per l'upgrade delle unità

--- app/src/main/java/com/example/thehillreloaded/Game/RecycleUnit.java
+++ app/src/main/java/com/example/thehillreloaded/Game/RecycleUnit.java
@@ -7,6 +7,10 @@ import android.graphics.Paint;
 import android.graphics.Point;
 
 public abstract class RecycleUnit {
+
+    protected static final int COST_OF_FIRST_UPGRADE = 4;
+    protected static final int COST_OF_SECOND_UPGRADE_RELOADED = 8;
+
     protected GameMode gameMode;
     protected Context context;
     protected TileMap map;
@@ -18,9 +22,8 @@ public abstract class RecycleUnit {
     protected int offsetFromLeft = 0;
     protected int offsetFromRight = 0;
     protected int myTiles[];
+    protected RecycleUnitStatus unitStatus = RecycleUnitStatus.BASE;
     protected ItemType acceptedItemType;
-
-    protected RecycleUnitStatus unitStatus;
 
     public RecycleUnit(TileMap map, Context context){
         this.gameMode = GameManager.getInstance().getGameMode();
@@ -39,9 +42,53 @@ public abstract class RecycleUnit {
             this.offsetFromRight = 0;
         }
     }
-
+    // Metodo per disegnare a schermo le unità di riciclo
     public void drawUnit(Canvas c){
         c.drawBitmap(sprite, position.x, position.y, null);
+    }
+
+    // Metodo per effettuare l'upgrade dell'unità
+    public boolean upgradeUnit(){
+        // Viene effettuato un controllo sullo stato in cui
+        // si trova l'unità
+        switch (unitStatus){
+            // Se l'unità è allo stato base e ha abbastanza
+            // unit points per effettuare l'upgrade
+            // cambia lo stato dell'unità e sottrae gli unit points necessari
+            case BASE:
+                if(unitPoints >= COST_OF_FIRST_UPGRADE){
+                    unitPoints -= COST_OF_FIRST_UPGRADE;
+                    unitStatus = RecycleUnitStatus.UPGRADED_ONCE;
+                    return true;
+                }
+                else return false;
+                // Se l'unità è già stata migliorata una volta
+                // controlla in che modalità ci si trova
+                // e effettua l'upgrade di conseguenza
+            case UPGRADED_ONCE:
+                if(gameMode == GameMode.CLASSIC){
+                    if(unitPoints >= COST_OF_FIRST_UPGRADE){
+                        unitPoints -= COST_OF_FIRST_UPGRADE;
+                        unitStatus = RecycleUnitStatus.UPGRADED_TWICE;
+                        return true;
+                    }
+                    else return false;
+                }else{
+                    if(unitPoints >= COST_OF_SECOND_UPGRADE_RELOADED){
+                        unitPoints -= COST_OF_SECOND_UPGRADE_RELOADED;
+                        unitStatus = RecycleUnitStatus.UPGRADED_TWICE;
+                        return true;
+                    }
+                    else return false;
+                }
+                // Se l'unità è stata già aggiornata
+                // due volte ritorna false poiché non
+                // è possibile aggiornarla oltre
+            case UPGRADED_TWICE:
+                return false;
+            default:
+                throw new IllegalStateException("Unexpected value: " + unitStatus);
+        }
     }
 
     // Getter e setter------------------------------------------------------------------------------
