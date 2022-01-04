d3e4970177c7492219b9bd6c53d8028572c5fb51
SabP391 <sabpinto91@gmail.com>
2022-01-04 16:40:31 +0100

    Fragment menu unità di riciclo vetro COMPLETAMENTE funzionante.

--- app/src/main/java/com/example/thehillreloaded/GlassUnitFragment.java
+++ app/src/main/java/com/example/thehillreloaded/GlassUnitFragment.java
@@ -1,8 +1,10 @@
 package com.example.thehillreloaded;
 
+import static com.example.thehillreloaded.Game.RecycleUnitStatus.BASE;
 import static com.example.thehillreloaded.Game.RecycleUnitStatus.UPGRADED_ONCE;
 import static com.example.thehillreloaded.Game.RecycleUnitStatus.UPGRADED_TWICE;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -14,9 +16,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.thehillreloaded.Game.GameAssets;
 import com.example.thehillreloaded.Game.RecycleUnitStatus;
 import com.example.thehillreloaded.Game.RecycleUnitsManager;
 
@@ -31,6 +35,20 @@ public class GlassUnitFragment extends Fragment {
     private boolean sbloccato2 = false;
     private boolean sbloccato3 = false;
     private boolean sbloccato4 = false;
+
+    //componenti layout
+    TextView costo1;
+    TextView costo2;
+    TextView costo3;
+    TextView costo4;
+    TextView ricavo1;
+    TextView ricavo2;
+    TextView ricavo3;
+    TextView ricavo4;
+    ImageButton sblocco1;
+    ImageButton sblocco2;
+    ImageButton sblocco3;
+    ImageButton sblocco4;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -96,54 +114,22 @@ public class GlassUnitFragment extends Fragment {
         unitWear.setText(getString(R.string.text_usura, wear, RecycleUnitsManager.getInstance().getGlassUnit().getMaximumWearLevel()));
 
         // Caratteristiche oggetti sbloccabili -----------------------------------------------------
-        TextView costo1 = (TextView) view.findViewById(R.id.glass_costo1);
-        TextView costo2 = (TextView) view.findViewById(R.id.glass_costo2);
-        TextView costo3 = (TextView) view.findViewById(R.id.glass_costo3);
-        TextView costo4 = (TextView) view.findViewById(R.id.glass_costo4);
-        TextView ricavo1 = (TextView) view.findViewById(R.id.glass_ricavo1);
-        TextView ricavo2 = (TextView) view.findViewById(R.id.glass_ricavo2);
-        TextView ricavo3 = (TextView) view.findViewById(R.id.glass_ricavo3);
-        TextView ricavo4 = (TextView) view.findViewById(R.id.glass_ricavo4);
-
-        costo1.setText(getString(R.string.text_costo_sbloccabili, RecycleUnitsManager.getInstance().getCostOfUnlockable1()));
-        costo1.setCompoundDrawablesWithIntrinsicBounds(0, 0 ,R.drawable.ic_unit_points_mini, 0);
-        costo1.setCompoundDrawablePadding(5);
-        ricavo1.setText(getString(R.string.text_ricavo_sbloccabili, RecycleUnitsManager.getInstance().getGainOfUnlockable1()));
-        ricavo1.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_sole_mini, 0);
-        ricavo1.setCompoundDrawablePadding(5);
-
-        if (RecycleUnitsManager.getInstance().getGlassUnit().getUnitStatus() == UPGRADED_ONCE) {
-            costo2.setText(getString(R.string.text_costo_sbloccabili, RecycleUnitsManager.getInstance().getCostOfUnlockable2()));
-            costo2.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_unit_points_mini, 0);
-            costo2.setCompoundDrawablePadding(5);
-            ricavo2.setText(getString(R.string.text_ricavo_sbloccabili, RecycleUnitsManager.getInstance().getGainOfUnlockable2()));
-            ricavo2.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_sole_mini, 0);
-            ricavo2.setCompoundDrawablePadding(5);
-        }
-
-        if (RecycleUnitsManager.getInstance().getGlassUnit().getUnitStatus() == UPGRADED_ONCE) {
-            costo3.setText(getString(R.string.text_costo_sbloccabili, RecycleUnitsManager.getInstance().getCostOfUnlockable3()));
-            costo3.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_unit_points_mini, 0);
-            costo3.setCompoundDrawablePadding(5);
-            ricavo3.setText(getString(R.string.text_ricavo_sbloccabili, RecycleUnitsManager.getInstance().getGainOfUnlockable3()));
-            ricavo3.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_sole_mini, 0);
-            ricavo3.setCompoundDrawablePadding(5);
-        }
-
-        if (RecycleUnitsManager.getInstance().getGlassUnit().getUnitStatus() == UPGRADED_TWICE) {
-            costo4.setText(getString(R.string.text_costo_sbloccabili, RecycleUnitsManager.getInstance().getCostOfUnlockable4()));
-            costo4.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_unit_points_mini, 0);
-            costo4.setCompoundDrawablePadding(5);
-            ricavo4.setText(getString(R.string.text_ricavo_sbloccabili, RecycleUnitsManager.getInstance().getGainOfUnlockable4()));
-            ricavo4.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_sole_mini, 0);
-            ricavo4.setCompoundDrawablePadding(5);
-        }
+        costo1 = (TextView) view.findViewById(R.id.glass_costo1);
+        costo2 = (TextView) view.findViewById(R.id.glass_costo2);
+        costo3 = (TextView) view.findViewById(R.id.glass_costo3);
+        costo4 = (TextView) view.findViewById(R.id.glass_costo4);
+        ricavo1 = (TextView) view.findViewById(R.id.glass_ricavo1);
+        ricavo2 = (TextView) view.findViewById(R.id.glass_ricavo2);
+        ricavo3 = (TextView) view.findViewById(R.id.glass_ricavo3);
+        ricavo4 = (TextView) view.findViewById(R.id.glass_ricavo4);
 
         // Sblocco oggetti in unità ----------------------------------------------------------------
-        ImageButton sblocco1 = (ImageButton) view.findViewById(R.id.glass_unlockable1);
-        ImageButton sblocco2 = (ImageButton) view.findViewById(R.id.glass_unlockable2);
-        ImageButton sblocco3 = (ImageButton) view.findViewById(R.id.glass_unlockable3);
-        ImageButton sblocco4 = (ImageButton) view.findViewById(R.id.glass_unlockable4);
+        sblocco1 = (ImageButton) view.findViewById(R.id.glass_unlockable1);
+        sblocco2 = (ImageButton) view.findViewById(R.id.glass_unlockable2);
+        sblocco3 = (ImageButton) view.findViewById(R.id.glass_unlockable3);
+        sblocco4 = (ImageButton) view.findViewById(R.id.glass_unlockable4);
+
+        setLayoutElements();
 
         sblocco1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -217,7 +203,9 @@ public class GlassUnitFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 if(RecycleUnitsManager.getInstance().getGlassUnit().upgradeUnit()){
-                    Toast.makeText(getActivity().getApplicationContext(), "Hai fatto cose.", Toast.LENGTH_SHORT).show();
+                    unitPoints.setText(getString(R.string.text_unit_points,
+                            RecycleUnitsManager.getInstance().getGlassUnit().getUnitPoints()));
+                    setLayoutElements();
                 } else {
                     Toast.makeText(getActivity().getApplicationContext(), "Non puoi fare cose.", Toast.LENGTH_SHORT).show();
                 }
@@ -239,5 +227,75 @@ public class GlassUnitFragment extends Fragment {
         ft.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
         ft.addToBackStack("fragment_sbloccabili");
         ft.commit();
+    }
+
+
+    public void setLayoutElements(){
+        Drawable tempSun = getActivity().getResources().getDrawable(R.drawable.ic_sole);
+        tempSun.setTint(getResources().getColor(R.color.verde_scuro));
+
+        sblocco1.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.asset3_unlockable_vetro_1));
+        sblocco1.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        sblocco1.setPadding(25,25,25,25);
+        costo1.setText(getString(R.string.text_costo_sbloccabili, RecycleUnitsManager.getInstance().getCostOfUnlockable1()));
+        costo1.setCompoundDrawablesWithIntrinsicBounds(0, 0 ,R.drawable.ic_unit_points_mini, 0);
+        costo1.setCompoundDrawablePadding(5);
+        ricavo1.setText(getString(R.string.text_ricavo_sbloccabili, RecycleUnitsManager.getInstance().getGainOfUnlockable1()));
+        ricavo1.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_sole_mini, 0);
+        ricavo1.setCompoundDrawablePadding(5);
+
+        if (RecycleUnitsManager.getInstance().getGlassUnit().getUnitStatus() == BASE) {
+            sblocco2.setImageDrawable(tempSun);
+            sblocco2.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            sblocco2.setPadding(25, 25, 25, 25);
+            sblocco3.setImageDrawable(tempSun);
+            sblocco3.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            sblocco3.setPadding(25, 25, 25, 25);
+            sblocco4.setImageDrawable(tempSun);
+            sblocco4.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            sblocco4.setPadding(25, 25, 25, 25);
+        }
+
+        if (RecycleUnitsManager.getInstance().getGlassUnit().getUnitStatus() == UPGRADED_ONCE) {
+            sblocco4.setImageDrawable(tempSun);
+            sblocco4.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            sblocco4.setPadding(25, 25, 25, 25);
+        }
+
+        if (RecycleUnitsManager.getInstance().getGlassUnit().getUnitStatus() == UPGRADED_ONCE
+                || RecycleUnitsManager.getInstance().getGlassUnit().getUnitStatus() == UPGRADED_TWICE ) {
+            sblocco2.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.asset3_unlockable_vetro_2));
+            sblocco2.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            sblocco2.setPadding(25,25,25,25);
+            costo2.setText(getString(R.string.text_costo_sbloccabili, RecycleUnitsManager.getInstance().getCostOfUnlockable2()));
+            costo2.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_unit_points_mini, 0);
+            costo2.setCompoundDrawablePadding(5);
+            ricavo2.setText(getString(R.string.text_ricavo_sbloccabili, RecycleUnitsManager.getInstance().getGainOfUnlockable2()));
+            ricavo2.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_sole_mini, 0);
+            ricavo2.setCompoundDrawablePadding(5);
+
+            sblocco3.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.asset3_unlockable_vetro_3));
+            sblocco3.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            sblocco3.setPadding(25,25,25,25);
+            costo3.setText(getString(R.string.text_costo_sbloccabili, RecycleUnitsManager.getInstance().getCostOfUnlockable3()));
+            costo3.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_unit_points_mini, 0);
+            costo3.setCompoundDrawablePadding(5);
+            ricavo3.setText(getString(R.string.text_ricavo_sbloccabili, RecycleUnitsManager.getInstance().getGainOfUnlockable3()));
+            ricavo3.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_sole_mini, 0);
+            ricavo3.setCompoundDrawablePadding(5);
+        }
+
+        if (RecycleUnitsManager.getInstance().getGlassUnit().getUnitStatus() == UPGRADED_TWICE) {
+            sblocco4.setImageDrawable(getActivity().getResources().getDrawable(R.drawable.asset3_unlockable_vetro_4));
+            sblocco4.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            sblocco4.setPadding(25,25,25,25);
+            costo4.setText(getString(R.string.text_costo_sbloccabili, RecycleUnitsManager.getInstance().getCostOfUnlockable4()));
+            costo4.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_unit_points_mini, 0);
+            costo4.setCompoundDrawablePadding(5);
+            ricavo4.setText(getString(R.string.text_ricavo_sbloccabili, RecycleUnitsManager.getInstance().getGainOfUnlockable4()));
+            ricavo4.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_sole_mini, 0);
+            ricavo4.setCompoundDrawablePadding(5);
+        }
+
     }
 }
